15dbe34f0e0ff0c092384097db1d5c99fae122de
hkcheckmate2 <47267569+hkcheckmate2@users.noreply.github.com>
2020-03-27 17:19:08 +0530

    Add files via upload

---
+++ prob0_proPos_probNeg.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr) {
+    
+    int sum0 = 0;
+    int sumpos = 0;
+    int sumneg = 0;
+    for (int i=0; i < arr.size(); i++)
+    {
+        if (arr[i]>0)
+        {
+            sumpos++;
+        }
+        else if (arr[i]<0)
+        {
+            sumneg++;
+        }
+        else
+        {
+            sum0++;
+        }
+    }
+    cout << fixed << setprecision(6) << float(sumpos)/arr.size() <<endl;
+    cout << fixed << setprecision(6) << float(sumneg)/arr.size() <<endl;
+    cout << fixed << setprecision(6) << float(sum0)/arr.size() <<endl;
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
